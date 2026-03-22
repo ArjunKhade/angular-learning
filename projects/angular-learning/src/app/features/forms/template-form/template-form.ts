f06b0d4c2748594a8d8b53131aed3782e117e4ab
ArjunKhade <khadearjun@gmail.com>
2026-03-22 15:22:05 +0530

    addded form save and validation logic

--- projects/angular-learning/src/app/features/forms/template-form/template-form.ts
+++ projects/angular-learning/src/app/features/forms/template-form/template-form.ts
@@ -1,44 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import {
-  NgxMaterialTimepickerComponent,
-  NgxMaterialTimepickerModule,
-} from 'ngx-material-timepicker';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-template-form',
-  imports: [CommonModule, NgxMaterialTimepickerModule, FormsModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-form.html',
   styleUrl: './template-form.css',
 })
 export class TemplateForm {
-  startTime: string | null = 'HH : MM AM';
-  endTime: string | null = 'HH : MM PM';
+  private form = viewChild.required<NgForm>('Loginform');
+  private destroyRef = inject(DestroyRef);
 
-  @ViewChild('startPicker') startPicker!: NgxMaterialTimepickerComponent;
-  @ViewChild('endPicker') endPicker!: NgxMaterialTimepickerComponent;
+  constructor() {
+    afterNextRender(() => {
+      const savedform = window.localStorage.getItem('saved-form');
 
-  openStartPicker() {
-    this.startPicker.open();
+      if (savedform) {
+        const data = JSON.parse(savedform);
+        const savedEmail = data.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => {
+            window.localStorage.setItem('saved-form', JSON.stringify({ email: value.email }));
+          },
+        });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
   }
 
-  handleStartTimeSet(time: string) {
-    this.startTime = time;
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) return;
+    console.log(formData.form.value.email);
+    console.log(formData.form.value.password);
+    console.log(formData.form);
 
-    // close start picker explicitly
-    this.startPicker.close();
-
-    // open end picker AFTER UI updates
-    setTimeout(() => {
-      this.endPicker.open();
-    }, 300);
-  }
-
-  handleEndTimeSet(time: string) {
-    this.endTime = time;
-    this.endPicker.close();
+    formData.form.reset();
   }
 }
